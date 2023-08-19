8596b3c1eec65e229b180966f881a4760224dce7
Sandhya327 <105123828+Sandhya327@users.noreply.github.com>
2023-08-19 11:22:41 +0530

    Add files via upload

---
+++ sending email/sending-emails-using-django/two/base/app1/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from app1.models import app1_customer
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+
+
+def index(request):
+    return render(request,'email.html')
+
+
+def sendMail(request):
+    # if request.method == 'Post':
+    firstName = request.POST['firstName']
+    secondName = request.POST['secondName']
+    email = request.POST['email']
+    message = request.POST['comment']
+    
+        #got data from frontend 
+    courtesy = "ThankYou for your feedback :) \n Will get in touch shortly"
+    message = "~~'\n" + message + "\n'~~" + " \n \n \n " + courtesy
+    # querry = app1_customer(firstName=firstName,secondName=secondName,email=email,message=message)
+    # querry.save()
+
+        # saved to db
+    print(firstName +"  "+ secondName)
+    send_mail(
+        'Feedback Form', #subject of the mail
+        message,
+        'sharanalwar57@gmail.com', # email addr of sender present in settings.py file
+        [email], #recievers mailID
+        fail_silently = True
+        )
+        #send to mail
+
+    return render(request , 'submit.html')
